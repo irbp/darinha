991ec13c701b75da4e35e0a714384e38ecc79b9c
Ítalo Paulino <italo3d@gmail.com>
2023-09-12 18:44:44 -0300

    first commit

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:rinha_de_compiler_dart/rinha_de_compiler_dart.dart';
+
+void main(List<String> arguments) {
+  print('Hello world: ${calculate()}!');
+}
---
+++ test/rinha_de_compiler_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:rinha_de_compiler_dart/rinha_de_compiler_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
