c2e1d8aeef16ddece4ef741728a324917368d346
Ítalo Paulino <italo3d@gmail.com>
2023-09-19 10:29:38 -0300

    use scoped memory

--- lib/interpreter.dart
+++ lib/interpreter.dart
@@ -15,34 +15,35 @@ import 'package:rinha_de_compiler_dart/ast/nodes/str_node.dart';
 import 'package:rinha_de_compiler_dart/ast/nodes/tuple_node.dart';
 import 'package:rinha_de_compiler_dart/ast/nodes/var_node.dart';
 import 'package:rinha_de_compiler_dart/ast/val.dart';
-import 'package:rinha_de_compiler_dart/call_stack.dart';
+
+typedef Memory = Map<String, Val>;
 
 class Interpreter {
   Interpreter({required AST ast}) : _ast = ast;
 
   final AST _ast;
-  final Map<String, CallStack> _memory = {};
+  final Memory _memory = {};
 
   void interpret() {
-    _visitNode(_ast.program);
+    _visitNode(_ast.program, _memory);
   }
 
-  Val _visitNode(Node node) {
+  Val _visitNode(Node node, Memory memory) {
     return switch (node) {
-      BoolNode() => _visitBoolNode(node),
-      IntNode() => _visitIntNode(node),
-      StrNode() => _visitStrNode(node),
-      PrintNode() => _visitPrintNode(node),
-      BinaryNode() => _visitBinaryNode(node),
-      LetNode() => _visitLetNode(node),
-      ParameterNode() => _visitParameterNode(node),
-      VarNode() => _visitVarNode(node),
-      IfNode() => _visitIfNode(node),
-      FunctionNode() => _visitFunctionNode(node),
-      CallNode() => _visitCallNode(node),
-      TupleNode() => _visitTupleNode(node),
-      FirstNode() => _visitFirstNode(node),
-      SecondNode() => _visitSecondNode(node),
+      BoolNode() => _visitBoolNode(node, memory),
+      IntNode() => _visitIntNode(node, memory),
+      StrNode() => _visitStrNode(node, memory),
+      PrintNode() => _visitPrintNode(node, memory),
+      BinaryNode() => _visitBinaryNode(node, memory),
+      LetNode() => _visitLetNode(node, memory),
+      ParameterNode() => _visitParameterNode(node, memory),
+      VarNode() => _visitVarNode(node, memory),
+      IfNode() => _visitIfNode(node, memory),
+      FunctionNode() => _visitFunctionNode(node, memory),
+      CallNode() => _visitCallNode(node, memory),
+      TupleNode() => _visitTupleNode(node, memory),
+      FirstNode() => _visitFirstNode(node, memory),
+      SecondNode() => _visitSecondNode(node, memory),
       _ => throw AssertionError(
           'file: ${node.location.filename}, '
           'position: ${node.location.start}: invalid node',
@@ -50,77 +51,75 @@ class Interpreter {
     };
   }
 
-  Val _visitBoolNode(BoolNode node) {
+  Val _visitBoolNode(BoolNode node, Memory memory) {
     return node.value;
   }
 
-  Val _visitIntNode(IntNode node) {
+  Val _visitIntNode(IntNode node, Memory memory) {
     return node.value;
   }
 
-  Val _visitStrNode(StrNode node) {
+  Val _visitStrNode(StrNode node, Memory memory) {
     return node.value;
   }
 
-  Val _visitPrintNode(PrintNode node) {
-    final val = _visitNode(node.value);
+  Val _visitPrintNode(PrintNode node, Memory memory) {
+    final val = _visitNode(node.value, memory);
     print(val);
     return val;
   }
 
-  Val _visitBinaryNode(BinaryNode node) {
-    final leftOperand = _visitNode(node.lhs);
-    final rightOperand = _visitNode(node.rhs);
+  Val _visitBinaryNode(BinaryNode node, Memory memory) {
+    final leftOperand = _visitNode(node.lhs, memory);
+    final rightOperand = _visitNode(node.rhs, memory);
     final binaryOp = node.op;
     return binaryOp.operate(leftOperand, rightOperand);
   }
 
-  Val _visitLetNode(LetNode node) {
-    final callStack = _memory[node.name.text] ?? CallStack();
-    final letValue = _visitNode(node.value);
-    callStack.push(letValue);
-    _memory[node.name.text] = callStack;
-    final nextValue = _visitNode(node.next);
-    callStack.pop();
-    return nextValue;
+  Val _visitLetNode(LetNode node, Memory memory) {
+    final letValue = _visitNode(node.value, memory);
+    final scopeMemory = Memory.from(memory);
+    scopeMemory[node.name.text] = letValue;
+    return _visitNode(node.next, scopeMemory);
   }
 
-  Val _visitParameterNode(ParameterNode node) {
+  Val _visitParameterNode(ParameterNode node, Memory memory) {
     return VoidVal();
   }
 
-  Val _visitVarNode(VarNode node) {
-    final value = _memory[node.text]?.peek();
+  Val _visitVarNode(VarNode node, Memory memory) {
+    final value = memory[node.text];
     if (value == null) {
       throw AssertionError("Variable with name '${node.text}' not found!");
     }
     return value;
   }
 
-  Val _visitIfNode(IfNode node) {
-    final conditionEvalued = _visitNode(node.condition);
+  Val _visitIfNode(IfNode node, Memory memory) {
+    final conditionEvalued = _visitNode(node.condition, memory);
     if (conditionEvalued is! BoolVal) {
       throw AssertionError('Condition should be a boolean expression!');
     }
     if (conditionEvalued.value) {
-      return _visitNode(node.then);
+      return _visitNode(node.then, memory);
     } else {
-      return _visitNode(node.otherwise);
+      return _visitNode(node.otherwise, memory);
     }
   }
 
-  Val _visitFunctionNode(FunctionNode node) {
+  Val _visitFunctionNode(FunctionNode node, Memory memory) {
     return ClosureVal(
       body: node.value,
       parameters: node.parameters,
     );
   }
 
-  Val _visitCallNode(CallNode node) {
-    final callee = _visitNode(node.callee);
+  Val _visitCallNode(CallNode node, Memory memory) {
+    final callee = _visitNode(node.callee, memory);
     if (callee is! ClosureVal) {
       throw AssertionError('Invalid function!');
     }
+
     final parametersLength = callee.parameters.length;
     final argumentsLength = node.arguments.length;
     if (parametersLength != argumentsLength) {
@@ -128,40 +127,35 @@ class Interpreter {
         '$parametersLength arguments expected, but $argumentsLength found.',
       );
     }
+
+    final scopeMemory = Memory.from(memory);
     for (int i = 0; i < parametersLength; i++) {
       final parameter = callee.parameters[i];
       final argument = node.arguments[i];
-      final callStack = _memory[parameter.text] ?? CallStack();
-      final value = _visitNode(argument);
-      callStack.push(value);
-      _memory[parameter.text] = callStack;
+      final value = _visitNode(argument, memory);
+      scopeMemory[parameter.text] = value;
     }
-    final calleeResult = _visitNode(callee.body);
-    for (int i = 0; i < parametersLength; i++) {
-      final parameter = callee.parameters[i];
-      final callStack = _memory[parameter.text] ?? CallStack();
-      callStack.pop();
-    }
-    return calleeResult;
+
+    return _visitNode(callee.body, scopeMemory);
   }
 
-  Val _visitTupleNode(TupleNode node) {
+  Val _visitTupleNode(TupleNode node, Memory memory) {
     return TupleVal(
-      first: _visitNode(node.first),
-      second: _visitNode(node.second),
+      first: _visitNode(node.first, memory),
+      second: _visitNode(node.second, memory),
     );
   }
 
-  Val _visitFirstNode(FirstNode node) {
-    final visitedNode = _visitNode(node.value);
+  Val _visitFirstNode(FirstNode node, Memory memory) {
+    final visitedNode = _visitNode(node.value, memory);
     if (visitedNode is! TupleVal) {
       throw AssertionError('first() function should be called with a tuple!');
     }
     return visitedNode.first;
   }
 
-  Val _visitSecondNode(SecondNode node) {
-    final visitedNode = _visitNode(node.value);
+  Val _visitSecondNode(SecondNode node, Memory memory) {
+    final visitedNode = _visitNode(node.value, memory);
     if (visitedNode is! TupleVal) {
       throw AssertionError('second() function should be called with a tuple!');
     }
