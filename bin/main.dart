655b9d247526dce84a97df79891f752421675ff1
Ítalo Paulino <italo3d@gmail.com>
2023-09-20 19:48:01 -0300

    Read filepath from arguments

--- bin/main.dart
+++ bin/main.dart
@@ -2,7 +2,11 @@ import 'package:rinha_de_compiler_dart/interpreter.dart';
 import 'package:rinha_de_compiler_dart/parser.dart';
 
 void main(List<String> arguments) {
-  final parser = Parser(filePath: './resources/main.json');
+  if (arguments.isEmpty) {
+    throw AssertionError('Filepath cannot be empty');
+  }
+  final filepath = arguments.first;
+  final parser = Parser(filePath: filepath);
   final ast = parser.parse();
   final interpreter = Interpreter(ast: ast);
   interpreter.interpret();
